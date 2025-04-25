0fedc76f88df533027e4e399ef7dfaba2a82450b
mirzababarhussain <48199676+mirzababarhussain@users.noreply.github.com>
2025-04-25 17:54:26 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Investment Fund Management</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
@@ -206,7 +206,7 @@
     <div class="container">
         <div class="login-container">
             <div class="login-logo">
-               <h3><b>Finance App</b></h3>
+               <h3><b>Investment Fund Management</b></h3>
             </div>
             <h4 style="text-align: center;">Login Area</h4>
             <form id="loginForm" novalidate>
